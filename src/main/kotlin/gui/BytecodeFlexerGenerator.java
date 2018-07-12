9f2880fbc2270e1f2731f71084761c4b8c3a9f0e
fredoverflow <fredoverflow@users.noreply.github.com>
2018-07-12 20:02:10 +0200

    remove methods operator and keyword

--- src/main/kotlin/gui/BytecodeFlexerGenerator.java
+++ src/main/kotlin/gui/BytecodeFlexerGenerator.java
@@ -2,13 +2,15 @@ package gui;
 
 public class BytecodeFlexerGenerator {
     public static void main(String[] args) {
-        new freditor.FlexerGenerator(-5, 3).generateTokens(
-                "@", "CODE", "MNEMONIC",
-                "RET",
-                "MOVE", "TRNL", "TRNA", "TRNR", "PICK", "DROP",
-                "BEEP", "HEAD", "LCLR", "FCLR", "RCLR",
-                "NOT", "AND", "OR", "XOR",
-                "PUSH", "LOOP", "CALL", "JUMP", "J0MP", "J1MP"
-        );
+        new freditor.FlexerGenerator(-4, 2)
+                .withIdentifierCall("END")
+                .generateTokens(
+                        "@", "CODE", "MNEMONIC",
+                        "RET",
+                        "MOVE", "TRNL", "TRNA", "TRNR", "PICK", "DROP",
+                        "BEEP", "HEAD", "LCLR", "FCLR", "RCLR",
+                        "NOT", "AND", "OR", "XOR",
+                        "PUSH", "LOOP", "CALL", "JUMP", "J0MP", "J1MP"
+                );
     }
 }
